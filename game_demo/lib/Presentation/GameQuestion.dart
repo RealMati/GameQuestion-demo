d3623f6131c07cd97b5b4eff8bba8ca8751d9ce6
Mati Milkessa <matimilkessa@gmail.com>
2024-06-26 14:46:41 +0300

    .

--- game_demo/lib/Presentation/GameQuestion.dart
+++ game_demo/lib/Presentation/GameQuestion.dart
@@ -3,33 +3,31 @@ import 'package:flutter/material.dart';
 class GameQuestion extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('MathJax in Flutter'),
-        ),
-        // body: Padding(
-        //   padding: const EdgeInsets.all(16.0),
-        //   child: Center(
-        //     child: Html(
-        //       data: '''<p>What are the roots of the quadratic equation
-        //       <span class="katex"><math xmlns="http://www.w3.org/1998/Math/MathML">
-        //       <semantics><mrow><msup><mi>x</mi><mn>2</mn></msup><mo>−</mo><mn>4</mn><mi>x</mi><mo>+</mo><mn>4</mn><mo>=</mo><mn>0</mn></mrow>
-        //       <annotation encoding="application/x-tex">x^2 - 4x + 4 = 0</annotation></semantics></math></span>?<br></p>''',
-        //       customRender: {
-        //         "math": (RenderContext context, Widget child) {
-        //           final String tex = context.tree.element!.text;
-        //           return Math.tex(
-        //             tex,
-        //             textStyle: TextStyle(fontSize: 24),
-        //           );
-        //         },
-        //       },
-        //       tagsList: Html.tags..add("math"),
-        //     ),
-        //   ),
-        // ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Question Page'),
       ),
+      // body: Padding(
+      //   padding: const EdgeInsets.all(16.0),
+      //   child: Center(
+      //     child: Html(
+      //       data: '''<p>What are the roots of the quadratic equation
+      //       <span class="katex"><math xmlns="http://www.w3.org/1998/Math/MathML">
+      //       <semantics><mrow><msup><mi>x</mi><mn>2</mn></msup><mo>−</mo><mn>4</mn><mi>x</mi><mo>+</mo><mn>4</mn><mo>=</mo><mn>0</mn></mrow>
+      //       <annotation encoding="application/x-tex">x^2 - 4x + 4 = 0</annotation></semantics></math></span>?<br></p>''',
+      //       customRender: {
+      //         "math": (RenderContext context, Widget child) {
+      //           final String tex = context.tree.element!.text;
+      //           return Math.tex(
+      //             tex,
+      //             textStyle: TextStyle(fontSize: 24),
+      //           );
+      //         },
+      //       },
+      //       tagsList: Html.tags..add("math"),
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
